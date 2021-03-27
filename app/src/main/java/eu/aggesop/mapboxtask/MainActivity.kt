d1bbe0cb34d71ae8b693058d1e97b5e54f0c4d3e
Tassilo Posegga <tassilo@posegga.eu>
2021-03-27 21:25:19 +0100

    install mapbox

--- app/src/main/java/eu/aggesop/mapboxtask/MainActivity.kt
+++ app/src/main/java/eu/aggesop/mapboxtask/MainActivity.kt
@@ -1,11 +1,28 @@
 package eu.aggesop.mapboxtask
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.Style
+import eu.aggesop.mapboxtask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync {
+            it.setStyle(Style.MAPBOX_STREETS) {
+                // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+            }
+        }
     }
+
+
 }
