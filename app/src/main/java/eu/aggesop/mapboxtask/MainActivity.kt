5045d9488b077b2e4ed542c15bf3325ca769cdab
Tassilo Posegga <tassilo@posegga.eu>
2021-03-27 21:50:29 +0100

    install places plugin, add lfiecyvle methods

--- app/src/main/java/eu/aggesop/mapboxtask/MainActivity.kt
+++ app/src/main/java/eu/aggesop/mapboxtask/MainActivity.kt
@@ -1,10 +1,14 @@
 package eu.aggesop.mapboxtask
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions
 import eu.aggesop.mapboxtask.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +21,66 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.mapView.onCreate(savedInstanceState)
-        binding.mapView.getMapAsync {
-            it.setStyle(Style.MAPBOX_STREETS) {
-                // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+        binding.mapView.getMapAsync { mapBoxMap ->
+            mapBoxMap.setStyle(Style.MAPBOX_STREETS) {
+                initSearchButton()
             }
         }
     }
 
+    private fun initSearchButton() {
+        binding.searchButton.setOnClickListener {
+            startPlacesPlugin()
+        }
+    }
 
+    private fun startPlacesPlugin() {
+        val placeOptions = PlaceOptions.builder()
+            .backgroundColor(Color.WHITE)
+            .build()
+        val intent = PlaceAutocomplete.IntentBuilder()
+            .accessToken(getString(R.string.mapbox_access_token))
+            .placeOptions(placeOptions)
+            .build(this)
+        startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    private companion object {
+        const val REQUEST_CODE_AUTOCOMPLETE = 1337
+    }
 }
